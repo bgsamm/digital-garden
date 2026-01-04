7e58c29f41d6ef7762e1a6b112e1b09fcffc86cd
Sean Miller <seanmiller999@gmail.com>
2026-01-04 11:55:20 -0700

    Add browser history support to page views

--- scripts/page.js
+++ scripts/page.js
@@ -1,4 +1,4 @@
-function setView(viewId) {
+function setView(viewId, updateHistory = true) {
     const targetView = document.getElementById(`view-${viewId}`);
 
     if (targetView === null) {
@@ -10,14 +10,35 @@ function setView(viewId) {
         return;
     }
 
+    // Remove active class from old tab & view
     const activeView = document.querySelector('.view.active');
     activeView.classList.remove('active');
 
     const activeTab = document.querySelector('.tab.active');
     activeTab.classList.remove('active')
 
+    // Add active class to new tab & view
     targetView.classList.add('active');
 
     const targetTab = document.getElementById(`tab-${viewId}`);
     targetTab.classList.add('active');
+
+    if (updateHistory) {
+        // Push new view onto browser history
+        const newUrl = window.location.pathname + '?view=' + viewId;
+        history.pushState({ view: viewId }, '', newUrl);
+    }
 }
+
+// Respond to browser back/forward buttons
+window.onpopstate = function (event) {
+    const viewId = (event.state && event.state.view) || 'main';
+    setView(viewId, updateHistory = false);
+};
+
+// Handle direct links
+window.addEventListener('load', () => {
+    const params = new URLSearchParams(window.location.search);
+    const viewId = params.get('view') || 'main';
+    setView(viewId);
+});
