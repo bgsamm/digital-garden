c430faa7f920d644f7a33a5e0c361c0379ef797a
Sean Miller <seanmiller999@gmail.com>
2026-01-21 21:06:28 +0000

    Implement task priority filtering

--- scripts/page.js
+++ scripts/page.js
@@ -1,8 +1,19 @@
-function updateDiffFilter(value) {
+function updateDiffFilter() {
+    const value = document.querySelector('input[name="diff-filter"]:checked').value;
     const taskList = document.getElementById('task-list');
     taskList.classList.remove('diff-easy');
     taskList.classList.remove('diff-med');
     taskList.classList.remove('diff-hard');
     taskList.classList.remove('diff-any');
     taskList.classList.add(`diff-${value}`);
+}
+
+function updatePrioFilter() {
+    const value = document.querySelector('input[name="prio-filter"]:checked').value;
+    const taskList = document.getElementById('task-list');
+    taskList.classList.remove('prio-low');
+    taskList.classList.remove('prio-mid');
+    taskList.classList.remove('prio-high');
+    taskList.classList.remove('prio-any');
+    taskList.classList.add(`prio-${value}`);
 }
